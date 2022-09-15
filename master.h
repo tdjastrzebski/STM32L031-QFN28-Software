4cac1a048c1ed8373af83edb34e3d8a108251140
Tomasz Jastrzębski <tomasz.jastrzebski@avanade.com>
2022-09-16 00:20:15 +0200

    initial version

---
+++ master.h
@@ -0,0 +1,23 @@
+#ifndef __MASTER_H
+#define __MASTER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// to be included in the main.c "USER CODE 1" section
+void PreInit(void);
+// to be included in the main.c "USER CODE Init" section
+void Init(void);
+// to be included in the main.c "USER CODE SysInit" section
+void SysInit(void);
+// to be included in the main.c "USER CODE 2" section
+void PostInit(void);
+// to be included in the main.c "USER CODE 3" section
+void MainLoop(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
